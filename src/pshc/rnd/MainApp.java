78981d51a30babd50930e671d5ceec6469d20276
jin <jin@polestarhc.com>
2018-03-15 11:30:56 +0900

    Fix main.

--- src/pshc/rnd/MainApp.java
+++ src/pshc/rnd/MainApp.java
@@ -1,5 +1,20 @@
 package pshc.rnd;
 
-public class MainApp {
+import java.io.File;
 
+import javax.swing.JFileChooser;
+
+public class MainApp {
+	
+	public static void main(String args[]) {
+		JFileChooser chooser = new JFileChooser();
+		
+		File file = new File("C:\\Users\\Jin\\Desktop/resultDicom.dcm");
+		
+	    String fileTypeName = chooser.getTypeDescription(file);   
+	    
+	    if (fileTypeName.equals("PACSPLUS")) {
+	    	System.out.println("File Type = " + fileTypeName);
+	    }
+	}
 }
--- src/pshc/rnd/exec/ExecuteTest.java
+++ src/pshc/rnd/exec/ExecuteTest.java
@@ -15,15 +15,16 @@ public class ExecuteTest {
 //		}
 		// 2 file open in cmd
 		try {
+			//Execute application
 			ProcessBuilder processBuilder = new ProcessBuilder(
 					"cmd","/c", "C:\\Users\\Jin\\Desktop/putty.exe");
 			processBuilder.start();
 					
 			Thread.sleep(1000);
 			
+			//Find process id from task manager
 			Runtime runtime1 = Runtime.getRuntime();
 			Process process1 = runtime1.exec("wmic process where name=\"putty.exe\" get processid");
-//			Process process = runtime1.exec("tasklist");
 			
 			BufferedReader br = new BufferedReader(new InputStreamReader(process1.getInputStream()));
 		    String tempString = "";
@@ -31,27 +32,28 @@ public class ExecuteTest {
 			while ((line = br.readLine()) != null) {
 				tempString += line;
 			}
-			tempString = tempString.replace(" ", "");
-		    System.out.println(tempString);
+			br.close();
 		    
 		    byte[] bytes = tempString.getBytes();
 		    String resultString = "";
-		    for (int i = 0; i < bytes.length; i++) {
+		    boolean isDone = false;
+		    for (int i = 0; i < bytes.length && !isDone; i++) {
 		    	if (bytes[i] > 47 && bytes[i] < 58) {
 		    		resultString += Character.toString((char)bytes[i]);
+		    	} else if (i > 0 && bytes[i-1] > 47 && bytes[i-1] < 58) {
+		    		isDone = true;
 		    	}
-		    	System.out.println(bytes[i]);
 		    }
+		    
 		    System.out.println(resultString);
 		    
-		    br.close();
 		    
-//		    Thread.sleep(2000);
 		    
-//			String tempPID = "4104";
-//			runtime1.exec("cmd /c TASKKILL /F /PID " + tempPID);
+		    Thread.sleep(2000);
+		    
+			runtime1.exec("cmd /c TASKKILL /F /PID " + resultString);
 			
-//			Thread.sleep(10000);
+			Thread.sleep(10000);
 			
 			
 		} catch (IOException e) {
